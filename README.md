d1e3e17c6d587bf04e092f3a4fa4e11d2f3ec8d0
Saumya Bhandary <saumyabhandary22@gmail.com>
2022-08-17 00:14:09 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Saumya-Bhandari" 
---
+++ script.js
@@ -0,0 +1,14 @@
+indexBody = document.getElementsByTagName("body")[0];
+
+
+// Function: addEventListener
+// Description: Adds an event listener to an element
+// Parameters: addEventListener(element, event, function)
+// Returns
+//on loading the index.html, load the mainpage.html
+
+
+
+window.onload = function() {
+    window.location.href = "mainpage.html";
+}
