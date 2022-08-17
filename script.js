416e9e1d44e89783673c80547f91c313e156840c
Saumya Bhandary <saumyabhandary22@gmail.com>
2022-08-17 13:42:13 +0545

    added the dialogue box

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 indexBody = document.getElementsByTagName("body")[0];
 
 
-// Function: addEventListener
-// Description: Adds an event listener to an element
-// Parameters: addEventListener(element, event, function)
-// Returns
-//on loading the index.html, load the mainpage.html
+function loadNavBar(){
+    document.getElementById("navbar").innerHTML = `
+    <!-- insert logo here -->
+    <a href="index.html" ><img src="images/logo.png" alt="logo" class="logo"></a>
+
+    <ul>
+        <li><a class="navButton1" href="index.html">Main Page</a></li>
+        <li><a class="navButton2" href="about.html">About Me</a></li>
+        <li><a class="navButton3" href="portfolio.html">My Projects</a></li>
+        <li><a class="navButton4" href="contact.html">C.V.</a></li>
+        <li><a class="navButton5" href="contact.html">Contact Me</a></li>
+        <li><a class="navButton6" href="contact.html">I Have a Blog Too!</a></li>
+    </ul>
+    `
+}
 
 
-
-window.onload = function() {
-    window.location.href = "mainpage.html";
+// open Main Page.html on loading window
+indexBody.onload = function() {
+    loadNavBar();
+    landingBody = document.getElementById("landing-page");
+    landingPageContent = document.getElementById('')
 }
