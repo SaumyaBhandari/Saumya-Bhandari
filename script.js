950f006e0734d0bfb43ec4c93ecc9660ddcbab9c
Saumya Bhandary <saumyabhandary22@gmail.com>
2022-08-17 23:26:18 +0545

    added the dialoguebox on homepage with animation

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-indexBody = document.getElementsByTagName("body")[0];
 
 
-function loadNavBar(){
-    document.getElementById("navbar").innerHTML = `
-    <!-- insert logo here -->
-    <a href="index.html" ><img src="images/logo.png" alt="logo" class="logo"></a>
+//landing page functions
 
-    <ul>
-        <li><a class="navButton1" href="index.html">Main Page</a></li>
-        <li><a class="navButton2" href="about.html">About Me</a></li>
-        <li><a class="navButton3" href="portfolio.html">My Projects</a></li>
-        <li><a class="navButton4" href="contact.html">C.V.</a></li>
-        <li><a class="navButton5" href="contact.html">Contact Me</a></li>
-        <li><a class="navButton6" href="contact.html">I Have a Blog Too!</a></li>
-    </ul>
-    `
-}
+//animations
 
-
-// open Main Page.html on loading window
-indexBody.onload = function() {
-    loadNavBar();
-    landingBody = document.getElementById("landing-page");
-    landingPageContent = document.getElementById('')
-}
+//create a funtion where mouse is over an element, the stype oroperty of other element changes from hidden to visible
+// function changeStyle() {
+//     var x = document.getElementById("dialogueBox");
+//     x.style.transitionDuration= '1s ease-out';
+//     if (x.style.visibility === "visible") {
+//         x.style.visibility = "hidden";
+//     } else {
+//         x.style.marginTop = "16vh";
+//         x.style.visibility= "visible";
+//     }
+// }
