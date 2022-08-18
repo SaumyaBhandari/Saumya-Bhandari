116fc05e997c5ea115cddbbe506821a7ca0a17bd
Saumya Bhandary <saumyabhandary22@gmail.com>
2022-08-18 08:15:11 +0545

    added about me

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 
 
-//landing page functions
-
-//animations
-
-//create a funtion where mouse is over an element, the stype oroperty of other element changes from hidden to visible
-// function changeStyle() {
-//     var x = document.getElementById("dialogueBox");
-//     x.style.transitionDuration= '1s ease-out';
-//     if (x.style.visibility === "visible") {
-//         x.style.visibility = "hidden";
-//     } else {
-//         x.style.marginTop = "16vh";
-//         x.style.visibility= "visible";
-//     }
-// }
